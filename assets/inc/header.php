c28666274b50c5c89cc69143fdfd58869e89f11b
hos4217 <hos4217@rit.edu>
2023-12-01 18:01:08 -0500

    Change from html to php and add include for header

---
+++ assets/inc/header.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>
+        <?php
+            echo "Learnix - " . $page;
+        ?>
+    </title>
+    <link rel="stylesheet" href="style.css" />
+    <script src="index.js"></script>
+  </head>
+
+  <body>
+
+    <div class="navcontainer">
+      <!-- Navigation start-->
+      <div class="leftside">
+        <div class="logo">
+          <!-- Logo class to make the dropdown menu-->
+          <img
+            src="media\learnix logo.png"
+            alt="Learnix Logo"
+            width="60px"
+            heigh="auto"
+          />
+
+          <div class="dropdown-menu">
+            <div class="dropdown-item">
+              <span class="circle"></span> Option 1
+            </div>
+            <div class="dropdown-item">
+              <span class="circle"></span> Option 2
+            </div>
+            <div class="dropdown-item">
+              <span class="circle"></span> Option 3
+            </div>
+            <div class="dropdown-item">
+              <span class="circle"></span> Option 4
+            </div>
+            <div class="dropdown-item">
+              <span class="circle"></span> Option 5
+            </div>
+            <div class="dropdown-item">
+              <span class="circle"></span> Option 6
+            </div>
+            <div class="dropdown-item">
+              <span class="circle"></span> Option 7
+            </div>
+          </div>
+        </div>
+        <!-- End of logo class -->
+
+        <ul class="menu">
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+          <li>Item 4</li>
+          <li>Item 5</li>
+        </ul>
+      </div>
+
+      <div class="rightside">
+        <div class="profilepic"></div>
+        <div class="user-profile">
+          <h6>Username</h6>
+        </div>
+      </div>
+    </div>
+    <!-- Navigation end-->
