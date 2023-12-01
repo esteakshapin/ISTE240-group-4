df4da21eeb91c8bb92d8c31e8a49fa5a0cf481ac
hos4217 <hos4217@rit.edu>
2023-12-01 18:28:34 -0500

    added content to lessons

--- lesson2.php
+++ lesson2.php
@@ -1,8 +1,63 @@
 <?php
-  $page = "Home";
+  $page = "Lesson 2";
   include("assets/inc/header.php");
 ?>
+      <div class="lessonplan">
+        <h3>Lesson Title</h3>
+        <div class="coverimage"></div>
+        <div class="lessontext"></div>
+        <div class="lessonquiz">
+          <h3>Lesson Quiz</h3>
+          <div class="question">
+            <h3>
+              Question 1: This a quiz testing you on what you've learned
+              throughout the lesson.
+            </h3>
+          </div>
+          <div class="quizcontainer">
+            <div class="quizbox">Option 1</div>
+            <div class="quizbox">Option 2</div>
+            <div class="quizbox">Option 3</div>
+            <div class="quizbox">Option 4</div>
+          </div>
+          <div class="carousel">
+            <div class="carouselitem"></div>
+            <div class="carouselitem"></div>
+            <div class="carouselitem"></div>
+            <div class="carouselitem"></div>
+            <div class="carouselitem"></div>
+          </div>
+        </div>
+      </div>
 
+      <div class="lessonplan">
+        <h3>Lesson 2</h3>
+        <div class="coverimage"></div>
+        <div class="lessontext"></div>
+        <div class="lessonquiz">
+          <h3>Lesson Quiz</h3>
+          <div class="question">
+            <h3>
+              Question 1: This a quiz testing you on what you've learned
+              throughout the lesson.
+            </h3>
+          </div>
+          <div class="quizcontainer">
+            <div class="quizbox">Option 1</div>
+            <div class="quizbox">Option 2</div>
+            <div class="quizbox">Option 3</div>
+            <div class="quizbox">Option 4</div>
+          </div>
+          <div class="carousel">
+            <div class="carouselitem"></div>
+            <div class="carouselitem"></div>
+            <div class="carouselitem"></div>
+            <div class="carouselitem"></div>
+            <div class="carouselitem"></div>
+          </div>
+        </div>
+      </div>
+    </div>
 <?php
   include("assets/inc/footer.php");
 ?>
