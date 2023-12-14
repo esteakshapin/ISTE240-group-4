db85b0887460f62be2f9bbc0b002d917de032daf
Uttechtt052 <tlh3486@g.rit.edu>
2023-12-14 18:38:52 -0500

    Fixed extra td issue

--- assets/inc/table-row.php
+++ assets/inc/table-row.php
@@ -1,5 +1,5 @@
 <tr class="<?php if ($counter % 2 != 0) {echo "alternate-color-tr";}?>">
-    <td>Test<td>
-    <td>Test</td>
-    <td>Test</td>
+    <td><?php echo $row['command'] ?></td>
+    <td><?php echo $row['description'] ?></td>
+    <td><?php echo $row['example'] ?></td>
 </tr>
