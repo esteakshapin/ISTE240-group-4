f4ef232a3be23ebde59f9906fed07e56ad5eeb63
Esteak Shapin <esteakshapin@gmail.com>
2023-12-14 17:28:33 -0500

    debugging mobile

--- allcommands.php
+++ allcommands.php
@@ -40,67 +40,13 @@ include("assets/inc/header.php");
   
   </table>
 
-  
-
-  <!-- <table>
-    <tr>
-      <th>Command</th>
-      <th>Description</th>
-      <th>Example</th>
-    </tr>
-    <tbody>
-      
-      <tr class="alternate-color-tr">
-        <td>pwd</td>
-        <td>Prints current working directory</td>
-        <td>pwd</td>
-      </tr>
-      <tr>
-        <td>ls</td>
-        <td>Prints files and folders in current working directory</td>
-        <td>ls -a<br>(a indicates tdat hidden files should be visible)</td>
-      </tr>
-      <tr>
-        <td>mv</td>
-        <td>Move or rename files. Botd initial file name and location and final file name and location must be
-          specified, so having a different final file name but tde same location essentially renames tde file in place.
-          Specifying a different final file location witd tde same name will move tde file while retaining its name.
-        </td>
-        <td>mv document.pdf ~/Documents/<br>(Moves document.pdf to tde user's documents folder. Does not change its
-          name)<br>mv document.pdf resume.pdf<br>(Renames document.pdf to resume.pdf but does not move file)</td>
-      </tr>
-      <tr>
-        <td>cp</td>
-        <td>Copies a file</td>
-        <td>cp document.pdf ~/Documents/documentcopy.pdf<br>(Copies document.pdf to tde user's documents folder under
-          tde name “documentcopy.pdf”</td>
-      </tr>
-      <tr>
-        <td>rm</td>
-        <td>Deletes a file or folder. tde -r flag can be used to delete a folder and its contents. Careful, tdere's no
-          recycling bin here by default so deletion is permanent! </td>
-        <td>rm document.pdf'<br>(deletes document.pdf)<br>rm -r ~/Documents<br>(deletes tde documents folder and
-          recursively deletes all files and subfolders inside. BE VERY CAREFUL.</td>
-      </tr>
-      <tr>
-        <td>chmod</td>
-        <td>Change access permissions on certain roles for a file or folder</td>
-        <td>chmod u+x script.sh<br>(Allows users (u) to execute (e) script.sh)</td>
-      </tr>
-      <tr>
-        <td>chown</td>
-        <td>Change ownership permissions for a file or folder</td>
-        <td>chown john document.pdf<br>(Gives user john ownership of document.pdf)</td>
-      </tr>
-    </tbody>
-  </table> -->
-
   <!-- mobile version -->
   <div class="mobile-command-section">
     <?php
       // looping thru the table and displaying each entry in an <li>
-    echo "inside mobile command - section";
+    
       if ($res) {
+        echo "inside mobile command - section";
         while ($row = mysqli_fetch_array($res, MYSQLI_ASSOC)) {
           include "./assets/inc/mobile-commands.php";
         }
