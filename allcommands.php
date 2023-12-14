7cded777425e96169c2c72e7fa035bee337126af
Esteak Shapin <esteakshapin@gmail.com>
2023-12-14 17:05:05 -0500

    added tr with command

--- allcommands.php
+++ allcommands.php
@@ -6,24 +6,36 @@ include("assets/inc/header.php");
 <div class="content">
   <h1>All Commands</h1>
 
-  <?php
-  include './dbConn.php';
-  // get all the comments in the database
-  if ($conn) {
-    // querying for all the comments in the database
-    $res = $conn->query('SELECT `command`, `description`, `example` FROM `commandStorage`');
+  <table>
+    <tr>
+      <th>Command</th>
+      <th>Description</th>
+      <th>Example</th>
+    </tr>
+    <tbody>
+      <?php
+    include './dbConn.php';
+    // get all the comments in the database
+    if ($conn) {
+      // querying for all the comments in the database
+      $res = $conn->query('SELECT `command`, `description`, `example` FROM `commandStorage`');
 
-    // looping thru the table and displaying each entry in an <li>
-    if ($res) {
-      while ($row = mysqli_fetch_array($res, MYSQLI_ASSOC)) {
-        echo $row['command'];
+      // looping thru the table and displaying each entry in an <li>
+      if ($res) {
+        while ($row = mysqli_fetch_array($res, MYSQLI_ASSOC)) {
+          echo "<tr><td>{$row['command']}</td></tr>";
+        }
+      } else {
+        echo "Error in query execution: " . mysqli_error($conn);
       }
-    } else {
-      echo "Error in query execution: " . mysqli_error($conn);
     }
-  }
 
-  ?>
+    ?>
+    </tbody>
+  
+  </table>
+
+  
 
   <!-- <table>
     <tr>
@@ -85,15 +97,16 @@ include("assets/inc/header.php");
   <!-- mobile version -->
   <div class="mobile-command-section">
     <?php
-    // looping thru the table and displaying each entry in an <li>
-    if ($res) {
-      while ($row = mysqli_fetch_array($res, MYSQLI_ASSOC)) {
-        include "./assets/inc/mobile-commands.php";
+      // looping thru the table and displaying each entry in an <li>
+    echo "inside mobile command - section";
+      if ($res) {
+        while ($row = mysqli_fetch_array($res, MYSQLI_ASSOC)) {
+          include "./assets/inc/mobile-commands.php";
+        }
+      } else {
+        echo "Error in query execution: " . mysqli_error($conn);
       }
-    } else {
-      echo "Error in query execution: " . mysqli_error($conn);
-    }
-    ?>
+      ?>
   </div>
 
 </div>
